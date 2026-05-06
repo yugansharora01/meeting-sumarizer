9654738cd52cf574d77dec3edd68624a74f3b1b6
yugansh arora <yugansharora01@gmail.com>
2026-05-06 12:10:48 +0530

    fresh code

--- content.js
+++
@@ -1,9 +0,0 @@
-console.log("Meeting page detected");
-
-if (window.location.hostname.includes("meet.google.com")) {
-  console.log("Google Meet detected");
-}
-
-if (window.location.hostname.includes("teams.microsoft.com")) {
-  console.log("Microsoft Teams detected");
-}
