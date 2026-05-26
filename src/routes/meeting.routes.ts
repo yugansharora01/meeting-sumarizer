b1f63967eb86a65b9781f39b884b59e6ea81a401
yugansh arora <yugansharora01@gmail.com>
2026-05-26 23:18:45 +0530

    deleted all code

--- src/routes/meeting.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { startMeetingBot } from "../controllers/meeting.controller";
-
-export const meetingRouter = Router();
-
-meetingRouter.post("/start-meeting-bot", startMeetingBot);
--- src/routes/webhook.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { handleRecallWebhook } from "../controllers/webhook.controller";
-
-export const webhookRouter = Router();
-
-webhookRouter.post("/webhook", handleRecallWebhook);
